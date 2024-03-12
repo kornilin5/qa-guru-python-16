f9f88a3a10b3778ddc3543808418830d97a82c8c
Kornilin5 <sasha_kornilin@mail.ru>
2024-03-12 18:01:29 +0400

    Добавлены изменения

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,5 +12,10 @@ def setup_browser(request):
         browser.config.base_url = "https://github.com/"
         browser.config.window_width = 480
         browser.config.window_height = 800
+    else:
+        browser.config.base_url = "https://github.com/"
+        browser.config.window_width = 1920
+        browser.config.window_height = 1080
+
     yield
     browser.quit()
--- tests/test_github_sign_in.py
+++ tests/test_github_sign_in.py
@@ -2,14 +2,20 @@ import pytest
 from selene import browser
 
 
-@pytest.mark.parametrize('setup_browser', ['desktop'], indirect=True)
+@pytest.mark.parametrize('setup_browser', ['desktop', 'mobile'], indirect=True)
 def test_open_github_sign_in_page_desktop():
-    browser.open('/')
-    browser.element('.HeaderMenu-link--sign-in').click()
+    if browser.config.window_width == 480 and browser.config.window_height == 800:
+        pytest.skip("Пропускаем тест так как размер desktop")
+    else:
+        browser.open('/')
+        browser.element('.HeaderMenu-link--sign-in').click()
 
 
-@pytest.mark.parametrize('setup_browser', ['mobile'], indirect=True)
+@pytest.mark.parametrize('setup_browser', ['desktop', 'mobile'], indirect=True)
 def test_open_github_sign_in_page_mobile():
-    browser.open('/')
-    browser.element('.Button--link .Button-content').click()
-    browser.element('.HeaderMenu-link--sign-in').click()
+    if browser.config.window_width == 1920 and browser.config.window_height == 1080:
+        pytest.skip("Пропускаем тест так как размер mobile")
+    else:
+        browser.open('/')
+        browser.element('.Button--link .Button-content').click()
+        browser.element('.HeaderMenu-link--sign-in').click()
